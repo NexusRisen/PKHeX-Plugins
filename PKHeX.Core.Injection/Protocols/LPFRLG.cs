663526e6d5b31eabde4b6dc05bf2010794bf9fef
santacrab2 <79347566+santacrab2@users.noreply.github.com>
2026-03-09 10:42:39 -0400

    minor clean

--- PKHeX.Core.Injection/Protocols/LPFRLG.cs
+++ PKHeX.Core.Injection/Protocols/LPFRLG.cs
@@ -5,8 +5,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Text.Json;
 using static PKHeX.Core.Injection.LiveHeXVersion;
 
 namespace PKHeX.Core.Injection;
@@ -16,7 +14,11 @@ public sealed class LPFRLG : InjectionBase
     public static ReadOnlySpan<LiveHeXVersion> SupportedVersions => [ FRLG_D_v100, FRLG_E_v100, FRLG_F_v100, FRLG_I_v100, FRLG_J_v100, FRLG_S_v100];
     private const uint fakeHeap = 0x2020000;
     private const uint startingOffset = 0x1208000;
+    private const int largeBlockSize = 0x3e00;
+    private const int offsetPointerSize = 4;
+    private const uint slotOffset = 4;
     public static uint securitykey = 0;
+
     public static uint GetB1S1Offset(LiveHeXVersion lv) => lv switch 
     { 
         LiveHeXVersion.FRLG_E_v100 => 0xBD68D2E0,
@@ -26,7 +28,8 @@ public sealed class LPFRLG : InjectionBase
         LiveHeXVersion.FRLG_F_v100 => 0xBD68D230,
         LiveHeXVersion.FRLG_J_v100 => 0xBD68D240,
     };
-    public uint GetLargeBlockOffset(LiveHeXVersion lv) => lv switch
+
+    public static uint GetLargeBlockOffset(LiveHeXVersion lv) => lv switch
     {
         LiveHeXVersion.FRLG_E_v100 => 0xBD68D2D8,
         LiveHeXVersion.FRLG_I_v100 => 0xBD68D228,
@@ -35,7 +38,8 @@ public sealed class LPFRLG : InjectionBase
         LiveHeXVersion.FRLG_F_v100 => 0xBD68D228,
         LiveHeXVersion.FRLG_J_v100 => 0xBD68D238,
     };
-    public uint GetSmallBlockOffset(LiveHeXVersion lv) => lv switch
+
+    public static uint GetSmallBlockOffset(LiveHeXVersion lv) => lv switch
     {
         LiveHeXVersion.FRLG_E_v100 => 0xBD68D2DC,
         LiveHeXVersion.FRLG_I_v100 => 0xBD68D22C,
@@ -45,63 +49,57 @@ public sealed class LPFRLG : InjectionBase
         LiveHeXVersion.FRLG_J_v100 => 0xBD68D23C,
         _ => 0,
     };
+
+    private static uint GetBoxOffset(PokeSysBotMini psb)
+    {
+        var boxoffbytes = psb.com.ReadBytes(GetB1S1Offset(psb.Version), offsetPointerSize);
+        var boxoff = BitConverter.ToUInt32(boxoffbytes);
+        boxoff -= fakeHeap;
+        boxoff += startingOffset;
+        return boxoff;
+    }
+
     public override Span<byte> ReadBox(PokeSysBotMini psb, int box, int len, List<byte[]> allpkm)
     {
-        var lv = psb.Version;
-        var boxoffbytes = psb.com.ReadBytes(GetB1S1Offset(lv), 4);
-        var boxoff = BitConverter.ToUInt32(boxoffbytes);
-        boxoff -= fakeHeap;
-        boxoff += startingOffset;
-        var boxsize = RamOffsets.GetSlotCount(lv) * RamOffsets.GetSlotSize(lv);
+        var boxoff = GetBoxOffset(psb);
+        var boxsize = RamOffsets.GetSlotCount(psb.Version) * RamOffsets.GetSlotSize(psb.Version);
         var boxstart = boxoff + (ulong)(box * boxsize);
-        return psb.com.ReadBytes(boxstart + 4, boxsize);
+        return psb.com.ReadBytes(boxstart + slotOffset, boxsize);
     }
+
     public override Span<byte> ReadSlot(PokeSysBotMini psb, int box, int slot)
     {
-        var lv = psb.Version;
-        var slotsize = RamOffsets.GetSlotSize(lv);
-        var boxoffbytes = psb.com.ReadBytes(GetB1S1Offset(lv), 4);
-        var boxoff = BitConverter.ToUInt32(boxoffbytes);
-        boxoff -= fakeHeap;
-        boxoff += startingOffset;
+        var boxoff = GetBoxOffset(psb);
+        var slotsize = RamOffsets.GetSlotSize(psb.Version);
         var slotstart = boxoff + (ulong)(slot * slotsize);
-        return psb.com.ReadBytes(slotstart + 4, slotsize);
+        return psb.com.ReadBytes(slotstart + slotOffset, slotsize);
     }
+
     public override void SendSlot(PokeSysBotMini psb, ReadOnlySpan<byte> data, int box, int slot)
     {
-        var lv = psb.Version;
-        var slotsize = RamOffsets.GetSlotSize(lv);
-        var boxoffbytes = psb.com.ReadBytes(GetB1S1Offset(lv), 4);
-        var boxoff = BitConverter.ToUInt32(boxoffbytes);
-        boxoff -= fakeHeap;
-        boxoff += startingOffset;
+        var boxoff = GetBoxOffset(psb);
+        var slotsize = RamOffsets.GetSlotSize(psb.Version);
         var slotstart = boxoff + (ulong)(slot * slotsize);
-        psb.com.WriteBytes(data, slotstart + 4);
+        psb.com.WriteBytes(data, slotstart + slotOffset);
     }
+
     public override void SendBox(PokeSysBotMini psb, ReadOnlySpan<byte> boxData, int box)
     {
-        var lv = psb.Version;
-        var boxoffbytes = psb.com.ReadBytes(GetB1S1Offset(lv), 4);
-        var boxoff = BitConverter.ToUInt32(boxoffbytes);
-        boxoff -= fakeHeap;
-        boxoff += startingOffset;
-        var boxsize = RamOffsets.GetSlotCount(lv) * RamOffsets.GetSlotSize(lv);
+        var boxoff = GetBoxOffset(psb);
+        var boxsize = RamOffsets.GetSlotCount(psb.Version) * RamOffsets.GetSlotSize(psb.Version);
         var boxstart = boxoff + (ulong)(box * boxsize);
-        psb.com.WriteBytes(boxData, boxstart + 4);
+        psb.com.WriteBytes(boxData, boxstart + slotOffset);
     }
     public override bool ReadBlockFromString(PokeSysBotMini psb, SaveFile sav, string block, out List<byte[]>? read)
     {
         read = null;
         try
         {
-            var offsets = SCBlocks[psb.Version].Where(z => z.Display == (block == "Large" ? "Items" : block)).First();
+            var offsets = SCBlocks[psb.Version].FirstOrDefault(z => z.Display == (block == "Large" ? "Items" : block))
+                ?? throw new KeyNotFoundException($"Block '{block}' not found for version {psb.Version}");
             var props = sav.GetType().GetProperty(block) ?? throw new Exception($"{block} not found");
-            var blockoffbytes = offsets.Name == "Large" ? psb.com.ReadBytes(GetLargeBlockOffset(psb.Version), 4) : psb.com.ReadBytes(GetSmallBlockOffset(psb.Version), 4);
-            var blockoff = BitConverter.ToUInt32(blockoffbytes);
-            blockoff -= fakeHeap;
-            blockoff += startingOffset;
-            blockoff += (uint)offsets.Offset;
-            var size = offsets.Name == "Large" ? 0x3e00 : Marshal.SizeOf(props.PropertyType);
+            var blockoff = GetBlockOffset(psb, offsets);
+            var size = offsets.Name == "Large" ? largeBlockSize : Marshal.SizeOf(props.PropertyType);
             var ram = psb.com.ReadBytes(blockoff, size);
             var val = ConvertValue(props, ram);
             if (offsets.IsSecured)
@@ -119,34 +117,40 @@ public sealed class LPFRLG : InjectionBase
             return false;
         }
     }
+
     public override void WriteBlocksFromSAV(PokeSysBotMini psb, string block, SaveFile sav)
     {
         var props = sav.GetType().GetProperty(block) ?? throw new Exception($"{block} not found");
-        var offsets = SCBlocks[psb.Version].Where(z => z.Display == (block == "Large" ? "Items" : block)).First();
+        var offsets = SCBlocks[psb.Version].FirstOrDefault(z => z.Display == (block == "Large" ? "Items" : block))
+            ?? throw new KeyNotFoundException($"Block '{block}' not found for version {psb.Version}");
         var data = block == "Large" ? ((SAV3)sav).Large.ToArray() : props.GetValue(sav);
         if (offsets.IsSecured)
             data = (uint)data ^ ((SAV3FRLG)sav).SecurityKey;
-        var blockoffbytes = offsets.Name == "Large" ? psb.com.ReadBytes(GetLargeBlockOffset(psb.Version), 4) : psb.com.ReadBytes(GetSmallBlockOffset(psb.Version), 4);
+        var blockoff = GetBlockOffset(psb, offsets);
+        psb.com.WriteBytes(block == "Large" ? (byte[])data : BitConverter.GetBytes((uint)data), blockoff);
+    }
+
+    private static uint GetBlockOffset(PokeSysBotMini psb, BlockData offsets)
+    {
+        var offsetAccessor = offsets.Name == "Large" ? GetLargeBlockOffset(psb.Version) : GetSmallBlockOffset(psb.Version);
+        var blockoffbytes = psb.com.ReadBytes(offsetAccessor, offsetPointerSize);
         var blockoff = BitConverter.ToUInt32(blockoffbytes);
         blockoff -= fakeHeap;
         blockoff += startingOffset;
         blockoff += (uint)offsets.Offset;
-        psb.com.WriteBytes(block == "Large" ? (byte[])data : BitConverter.GetBytes((uint)data), blockoff);
+        return blockoff;
     }
     public object ConvertValue(PropertyInfo info, Span<byte> bytes)
     {
         var t = info.PropertyType;
-        if (t == typeof(UInt16))
-            return BitConverter.ToUInt16(bytes);
-        else if (t == typeof(UInt32))
-            return BitConverter.ToUInt32(bytes);
-        else if (t == typeof(UInt64))
-            return BitConverter.ToUInt64(bytes);
-        else if (t == typeof(UInt128))
-            return BitConverter.ToUInt128(bytes);
-        else
-            return bytes.ToArray();
-        throw new InvalidEnumArgumentException($"Unsupported type {t} for {info.Name}");
+        return t switch
+        {
+            var _ when t == typeof(UInt16) => BitConverter.ToUInt16(bytes),
+            var _ when t == typeof(UInt32) => BitConverter.ToUInt32(bytes),
+            var _ when t == typeof(UInt64) => BitConverter.ToUInt64(bytes),
+            var _ when t == typeof(UInt128) => BitConverter.ToUInt128(bytes),
+            _ => bytes.ToArray(),
+        };
     }
     private static BlockData Get(uint offset, string name, string display, SCTypeCode type) => new()
     {
@@ -177,8 +181,12 @@ public sealed class LPFRLG : InjectionBase
     };
     public static readonly Dictionary<LiveHeXVersion, BlockData[]> SCBlocks = new()
     {
-        { FRLG_E_v100, Blocks_FRLG  },
+        { FRLG_D_v100, Blocks_FRLG },
+        { FRLG_E_v100, Blocks_FRLG },
+        { FRLG_F_v100, Blocks_FRLG },
         { FRLG_I_v100, Blocks_FRLG },
+        { FRLG_J_v100, Blocks_FRLG },
+        { FRLG_S_v100, Blocks_FRLG },
     };
     public override Dictionary<string, string> SpecialBlocks { get; } = new()
     {
